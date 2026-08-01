add3a9d4dc9d9f51f4032bf88ca93ac65c1c023f
Lowen <lowenhsu.tw@gmail.com>
2026-08-01 14:37:04 +0800

    Route app through universal bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import UniversalApp from "./UniversalApp.jsx";
 import "./index.css";
-ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><App /></React.StrictMode>);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <UniversalApp />
+  </React.StrictMode>,
+);
